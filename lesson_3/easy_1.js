a878db535c89063c3dcd8ee5a62f68a39ebfd32d
Kai <rdo9313@gmail.com>
2023-09-09 04:49:01 -0400

    easy_1 exercises

---
+++ lesson_3/easy_1.js
@@ -0,0 +1,51 @@
+// 1
+// No error. numbers[4] will output undefined.'
+
+// 2
+// let str1 = "Come over here!"; // true
+// let str2 = "What's up, Doc?"; // false
+
+// str1.endsWith("!");
+// str2.endsWith("!");
+
+// 3
+// ages.hasOwnProperty("Spot");
+
+// 4
+// let munstersDescription = "the Munsters are CREEPY and Spooky.";
+// // => The munsters are creepy and spooky.
+// console.log(
+//   munstersDescription[0].toUpperCase() +
+//     munstersDescription.slice(1).toLowerCase()
+// );
+
+// 5
+// true, False
+
+// 6
+// let ages = { Herman: 32, Lily: 30, Grandpa: 5843, Eddie: 10 };
+// let additionalAges = { Marilyn: 22, Spot: 237 };
+// console.log(Object.assign(ages, additionalAges));
+
+// 7
+// let str1 =
+//   "Few things in life are as important as house training your pet dinosaur.";
+// let str2 = "Fred and Wilma have a pet dinosaur named Dino.";
+
+// console.log(str1.includes("Dino"));
+// console.log(str2.includes("Dino"));
+
+// 8
+// let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
+// flintstones.push("Dino"), flintstones = flintstones.concat("Dino")
+
+// 9
+// flintstones.push("Dino", "Hoppy");
+
+// 10
+// let advice =
+//   "Few things in life are as important as house training your pet dinosaur.";
+
+// // Expected return value:
+// // => 'Few things in life are as important as '
+// console.log(advice.slice(0, advice.indexOf("house")));
